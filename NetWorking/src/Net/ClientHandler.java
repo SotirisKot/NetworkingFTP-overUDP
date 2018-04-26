1cddfb96bebef38ba67efa9280d0b9758a60b390
GeorgeChatzo <giorgoschatzopoulos7@gmail.com>
2018-04-26 15:24:23 +0300

    "comments added"

--- NetWorking/src/Net/ClientHandler.java
+++ NetWorking/src/Net/ClientHandler.java
@@ -8,6 +8,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
+/*
+* ClientHandler: Η κλάση αυτή είναι ένα thread που τρέχει η Client και διευκολύνει την
+* επικοινωνία μεταξύ του client και του server και είναι η κλάση η οποία λαμβάνει και στέλνει πακέτα απο και προς τον client.
+* */
+
 public class ClientHandler extends Thread{
     private String filePath;
     private String fileName,extension;
@@ -17,7 +22,7 @@ public class ClientHandler extends Thread{
     private String state = "none";
     private int SYNC_NUM = 0,sequence_num=0;
     private int maxPayload;
-    private int defaultPayload = 1024;
+    private int defaultPayload = 1024; //έχουμε θέσει εμείς default μέγεθος του buffer που θα αποθηκεύει τα πακέτα ack,syn_ack,sync
     private int PacketCounter;
 
   public ClientHandler(InetAddress address, int portNumber, String filePath, String fileName, String extension, int maxPayload){
@@ -29,11 +34,13 @@ public class ClientHandler extends Thread{
     this.maxPayload = maxPayload;
   }
 
+//Ξεκινάει η εκτέλεση του thread
   public void run() {
+      //Για να ξεκινήσει το 3 way handshake πρέπει ο client να στείλει ένα sync πακέτο προς τον server πράγμα που γίνεται παρακάτω
       Packet syncPacket = new Packet();
       syncPacket.setSynPacket(true);
       syncPacket.setSynNum(SYNC_NUM);
-      syncPacket.setData(Integer.toString(maxPayload));
+      syncPacket.setData(Integer.toString(maxPayload)); //με το ack πακέτο περνάμε στον server το maxPayload που έσδωσε ο χρήστης
       try {
           socket = new DatagramSocket();
           sendData(syncPacket.toString());
@@ -49,29 +56,34 @@ public class ClientHandler extends Thread{
           if (state.equals("sync_sent")) {
               try {
                   socket.receive(packet);
-                  Packet p = Packet.processingData(new String(buffer));
+                  Packet p = Packet.processingData(new String(buffer));//Κάνουμε process τα στοιχεία του πακέτου που μόλις παραλάβαμε
                   System.out.println("Received: " + p.toString());
                   if (p.getAckNum() == SYNC_NUM) {//ack packet must be valid!!!
                       System.out.println("3-way handshake step 2...Now will send ack back to server to establish" +
                               " connection!!!");
 
-                      sendAck(p.getSynNum());
+                      sendAck(p.getSynNum());//Έχουμε παραλάβει ack_sync και στέλνουμε ack
                       System.out.println("3-way handshake finished...connection established.");
-                      state = "connection_established";
+                      state = "connection_established";//το connection εγκαθιδρύθηκε 3 way handshake ολοκληρώθηκε
                   }
               } catch (IOException e) {
                   e.printStackTrace();
               }
           } else if (state.equals("connection_established")) {
-
+              //Αφού ολοκληρώθηκε το handshake ξεκινάμε το process του αρχείου που μας έδωσε ο χρήστης σαν Input.
               Path path = Paths.get(filePath);
               try {
+                  //Κάνουμε read όλα τα bytes απο το αρχείο μας
                   byte[] buf = Files.readAllBytes(path);
                   long startTime = System.nanoTime();
+                  //Θέτουμε χρόνο εκκίνησης
                   System.out.println(startTime + " NSEC");
+                  //Ξεκινάμε μεταφορά αρχείου
                   transferFile(buf, address, portNumber);
+                  //θέτουμε χρόνο τέλους και μετά υπολογίζουμε το χρόνο που διήρκησε η μεταφορά του αρχείου
                   long endTime = System.nanoTime();
                   System.out.println(endTime + " NSEC" + "\n diff " + (endTime - startTime));
+                  //Παράγουμε στατιστικά με βάση το χρόνο μεταφοράς αριθμό πακέτων που στάλθηκαν και το payload του χρήστη.
                   getStatistics(startTime, endTime, PacketCounter, maxPayload);
                   complete = true;
               } catch (IOException e) {
@@ -82,9 +94,11 @@ public class ClientHandler extends Thread{
 
       }
   }
-
+  //transferFile: Χρησιμοπιείται για την μεταφορά του αρχείου απο τον Client στον Server
   private void transferFile(byte[] buffer, InetAddress address, int portNumber) throws IOException {
       String state = "packet_send";
+      //Θέτουμε το load να είναι maxPayload - 8 δίοτι έχω συνολικά 8 bytes ένα για το header και 7 για το data.
+      // Άρα αν το payload είναι 65500 τότε θα έχω load = 65492.
       int load = maxPayload-8;
       boolean file_sent = false;
       boolean last_packet = false;
@@ -92,13 +106,20 @@ public class ClientHandler extends Thread{
       int start=0;
       byte[] sendBuf = new byte[load];
       System.out.println("The file is: "+ buffer.length + " bytes!!");
+      //Θέτουμε timeout 1500 όπως έχει διευκρινιστεί απο την εκφώνηση της άσκησης
       int timeout = 1500;
       System.out.println("The timeout will be: " + timeout + "ms");
+      //Ξεκινάει η μεταφορά του αρχείου όσο το file_sent = false
       while (!file_sent){
           ByteArrayOutputStream stream = new ByteArrayOutputStream();
           DataOutputStream out = new DataOutputStream(stream);
+          //Σε περίπτωση που θέλω να στείλω κάποιο πακέτο βλέπω το state του. Αν το state είναι packet_send πάω και στέλνω πακέτο
           if(state.equals("packet_send")){
               if(!send_again){
+                  //Αν το πακέτο που στέλνουμε δεν το έχουμε ξαναστείλει τότε θα ξεκινήσω να γεμίζω τον sendBuf μέσα απο τον
+                  // buffer που περνάω στην μέθοδο ο οποίος περιέχει τα Bytes
+                  //  του πακέτου που έδωσα. Χρησιμοποίουμε την μεταβλητή start για να ξέρω κάθε φορά απο ποιον αριθμό του πίνακα Buffer
+                  // θα συνεχίσω να γεμίσω και το επόμενο sendBuf.
                   for(int i=0; i<sendBuf.length; i++){
                       sendBuf[i] = buffer[start];
                       start++;
@@ -108,6 +129,8 @@ public class ClientHandler extends Thread{
                       }
                   }
               }
+              //Άν είμαι στο τελευταίο πακέτο τότε θα γράψω στο datagramPacket που θα στείλω στον server,
+              // τον αριθμό 2 έτσι ώστε να ξέρω ότι η μεταφορά του αρχείου ολοκληρώθηκε.
               if(last_packet){
                   out.writeInt(2);
                   out.writeInt(load);
@@ -121,6 +144,7 @@ public class ClientHandler extends Thread{
                   out.close();
                   stream.close();
               }else{
+                  //Σε περίπτωση που δεν είναι τελευταίο πακέτο το στέλνω κανονικά στον server περιμένοντας για το ack
                   out.writeInt(sequence_num);
                   out.writeInt(load);
                   out.write(sendBuf);
@@ -133,14 +157,17 @@ public class ClientHandler extends Thread{
                   out.close();
                   stream.close();
               }
+              //Σε περίπτωση που έχω ήδη στείλει το πακέτο και περιμένω να λάβω ack, θέτω timeout και κατόπιν περιμένω να λάβω το ack.
+              // Αν το λάβω τότε προχωράω στην αποστολή επόμενου πακέτου αλλίως αν δεν το λάβω προκαλείται timeout και πρέπει
+              // να ξαναστείλω το ack του πακέτου.
           }else if(state.equals("wait_ack")){
+
               byte[] bufferReceive = new byte[defaultPayload];
               DatagramPacket packetAck = new DatagramPacket(bufferReceive,bufferReceive.length);
               try{
                   socket.setSoTimeout(timeout);
                   socket.receive(packetAck);
                   Packet p = Packet.processingData(new String(bufferReceive));
-
                   if(p.getAckPacket() && p.getAckNum() == sequence_num){
                       if(sequence_num == 1){
                           sequence_num--;
@@ -166,13 +193,14 @@ public class ClientHandler extends Thread{
       }
   }
 
-
+    //sendData: Την χρησιμοποιούμε προκειμένου να στείλουμε data στον Server μέσω ενός DatagramPacket
     private void sendData(String data) throws IOException {
         byte[] buffer = data.getBytes();
         DatagramPacket p = new DatagramPacket(buffer,buffer.length,address,portNumber);
         socket.send(p);
     }
 
+    //getStatistics: Παράγουμε στατιστικά για το σύνολο των πακέτων που μεταφέρθηκαν , το χρόνο που έκαναν να σταλούν και το payload του χρήστη
     private void getStatistics(long startTime,long endTime,int PacketCounter,int maxPayload){
         long totalBytes = PacketCounter*maxPayload;
         long totalTime = endTime - startTime;
@@ -184,6 +212,7 @@ public class ClientHandler extends Thread{
         System.out.println("The payload was: "+ maxPayload);
     }
 
+    //sendAck: Την χρησιμοποιούμε για να στείλουμε ack για το πακέτο που μόλις παραλάβαμε μέσω της sendData
     private void sendAck(int ackNum) throws IOException {
         Packet ack = new Packet();
         ack.setAckPacket(true);
--- NetWorking/src/Net/Server.java
+++ NetWorking/src/Net/Server.java
@@ -7,6 +7,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
+
+/*
+* Server Class: Η κλάση αυτή είναι ο Server μας με τον οποίο κάνουν connect όλοι οι clients.
+* O server ζητάει απο τον χρήστη να του δώσει την ip και ένα Port πάνω στα οποία θα μπορεί ο εκάστοτε client να συνδεθεί
+* και αφου ολοκληρωθεί η σύνδεση μεταξύ των 2 ξεκινάει το 3 way handshake.
+* */
+
+
 public class Server {
 
    private String ServerIP;
@@ -42,6 +50,8 @@ public class Server {
     /*
     It will handle the communication with the client..receives a packet and waits for a certain amount of time
     before it sents back an ack.
+    Η communicationHandler είναι η μέθοδος που χειρίζεται το connection με τον client. Λαμβάνει πακέτο και περιμένει λίγο
+    πρωτού στείλει ack πίσω.
      */
     private void communicationHandler(String state){
         try {
@@ -56,6 +66,8 @@ public class Server {
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             System.out.println("Waiting for a request!!!");
             try {
+                //Λαμβάνει πακέτο και μέσω της Proccesing data Που έχουμε στην Packet
+                // αναλύουμε το πακέτο στα επιμέρους στοιχεία του
                 socket.receive(packet);
                 System.out.println("Got a request!!!");
                 Packet p = Packet.processingData(new String(buf));
@@ -65,8 +77,8 @@ public class Server {
                 }
                 InetAddress clientAddress = packet.getAddress();//gets the address and the port of the client.
                 int clientPort = packet.getPort();
-                if(state.equals("none")){//we wait for a syn packet..to start the handshake...
-                    if(p.getSynPacket()){
+                if(state.equals("none")){//Περιμένουμε πακέτο για να ξεκινήσει το handshake
+                    if(p.getSynPacket()){//Τσεκάρουμε αν το πακέτο που λάβαμε είναι syn_packet
                         System.out.println("Client initialized 3-way handshake!!!");
                         Packet response = new Packet();
                         response.setSynPacket(true);
@@ -74,24 +86,28 @@ public class Server {
                         response.setSynNum(SYNC_NUM);
                         response.setAckNum(p.getSynNum());
                         sendData(response.toString(),clientAddress,clientPort);
-                        state = "syn_received";
+                        state = "syn_received";//Σε περίπτωση που πράγματι είναι syn το πακέτο στέλνουμε πίσω
+                                               // στον client ενα syn_ack για να τον ενημερωσουμε ότι λάβαμε το syn packet.
+                        // Για να το επιτύχουμε αυτό θέτουμε τα πεδία SynPacket Και AckPacket σε true του packet που στέλνουμε
+                        // και αλλάζουμε το state σε syn_received
                         System.out.println("3-way handshake step 2: sent ack-syn packet waiting for ack...");
                     }
-                }else if(state.equals("syn_received")){
-                    if(p.getAckPacket() && p.getAckNum() == SYNC_NUM){
+                }else if(state.equals("syn_received")){//εδώ ελέγχουμε αν όντως έχουμε λάβει syn packet
+                    if(p.getAckPacket() && p.getAckNum() == SYNC_NUM){ //Τσεκάρουμε αν αυτό που λάβαμε είναι ack packet και αν
+                        // πράγματι είναι τότε κάνουμε το state established και έχουμε εγκαθιδρύσει το 3 way handshake.
                         state = "established";
                         System.out.println("3-way handshake completed...CONNECTION ESTABLISHED!!!");
                         try {
-                            file = new FileOutputStream("FTPresult" + i);
+                            file = new FileOutputStream("FTPresult" + i);//Το τελικό αρχείο που προκύπτει
                         }catch (FileNotFoundException e) {
                             e.printStackTrace();
                         }
                     }
-                }else if(state.equals("established")){
-                    //if packet is valid..opening the file and converting it to byte array so we can send it to client.
+                }else if(state.equals("established")){//όταν πια έχω ολοκληρώσει το 3 way με επιτυχία
+                    //Αν το πακέτο είναι έγκυρο..ανοίγω το αρχείο και το μετατρέπω σε byteArray ώστε να το στείλουμε στον client.
                     boolean completed = false;
-                    while(!completed) {
-                        byte[] buffer = new byte[maxPayload];
+                    while(!completed) {//γίνεται μέχρι να λάβει όλα τα πακέτα ουσιαστικά μέχρι να παραληφθεί όλο το αρχείο
+                        byte[] buffer = new byte[maxPayload];//buffer μεγέθους payload που δίνει ο χρήστης και λαμβάνει ένα πακέτο την φορά
                         DatagramPacket packet1 = new DatagramPacket(buffer, buffer.length);
                         try {
                             socket.setSoTimeout(0);
@@ -105,13 +121,13 @@ public class Server {
                                 in.read(data);
                                 file.write(data);
                                 causeDelay();
-                                sendAck(sequence_number,clientAddress,clientPort);
-                                if(sequence_num == 1){
+                                sendAck(sequence_number,clientAddress,clientPort);//στέλνουμε ack ότι λάβαμε το πακέτο πίσω στον client
+                                if(sequence_num == 1){//αλλάζουμε το seq_number
                                     sequence_num--;
                                 }else if(sequence_num == 0){
                                     sequence_num++;
                                 }
-                            }else if(sequence_number==2){
+                            }else if(sequence_number==2){//το seq_number γίνεται 2 μόνο όταν έχει ολοκληρωθεί η μεταφορά του αρχείου
                                 System.out.println("File has been transferred!!");
                                 int packetLength = in.readInt();
                                 byte[] data = new byte[packetLength];
@@ -129,7 +145,8 @@ public class Server {
                                 i++;
                                 sequence_num = 0;
                             }else{
-                                System.out.println("Packet is a duplicate...just send ack!!!");
+                                System.out.println("Packet is a duplicate...just send ack!!!");//σε περίπτωση που έχουμε λάβει το πακέτο
+                                // αλλά δεν έχουμε στείλει ack. Οπότε το πακέτο που λάβαμε είναι duplicate και στέλνουμε πίσω μόνο το ack του.
                                 causeDelay();
                                 sendAck(sequence_number,clientAddress,clientPort);
                             }
@@ -147,6 +164,7 @@ public class Server {
 
     }
 
+    //sendAck: Την χρησιμοποιούμε για να στείλουμε ack για το πακέτο που μόλις παραλάβαμε μέσω της sendData
     private void sendAck(int ackNum,InetAddress address,int clientPort) throws IOException {
         Packet ack = new Packet();
         ack.setAckPacket(true);
@@ -154,19 +172,22 @@ public class Server {
         sendData(ack.toString(),address,clientPort);
     }
 
-
+    //sendData: Την χρησιμοποιούμε προκειμένου να στείλουμε data στον client μέσω ενός DatagramPacket
     private void sendData(String data,InetAddress address,int clientPort) throws IOException {
         byte[] buffer = data.getBytes();
         DatagramPacket p = new DatagramPacket(buffer,buffer.length,address,clientPort);
         socket.send(p);
     }
 
+    //causeDelay: η διαδικασία
+    //παραγωγής επιβεβαίωσης με την εισαγωγή τεχνητής καθυστέρησης, η οποία ακολουθεί
+    //εκεθετικής κατανομή
     private void causeDelay() throws InterruptedException {
       double timeout = getTimeout();
       long timeoutTime = (long) timeout;
       Thread.sleep(timeoutTime);
     }
-
+    //getTimeout: υπολογισμός του τεχνητού Timeout
     private double getTimeout() {
       Random rand = new Random();
       double lambda = 1/4.2;
