496bf21a03a708d53e7e30a8d75c9bc6396283be
GeorgeChatzo <giorgoschatzopoulos7@gmail.com>
2018-04-26 11:35:24 +0300

    "new entries"

--- NetWorking/src/Net/Client.java
+++ NetWorking/src/Net/Client.java
@@ -4,18 +4,22 @@ import java.io.*;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import static java.lang.System.exit;
+
 public class Client {
 
 
   public static void main(String args[]) throws IOException {
 
-      String serverIP;
-      int portNumber;
-      String fileName;
-      String filePath;
-      String extension;
-      int maxPayload;
-      InetAddress address;
+      String serverIP = "";
+      int portNumber = 0;
+      String fileName = "";
+      String filePath = "";
+      String extension = "";
+      int maxPayload = 0;
+      InetAddress address = InetAddress.getByName(serverIP);
+      String finalPath = "";
+      boolean input = false;
 
       if(args.length == 0){
           System.out.println("Wrong Arguments!!!");
@@ -26,27 +30,64 @@ public class Client {
           portNumber = Integer.parseInt(br.readLine());
           System.out.println("Give file name");
           fileName = br.readLine();
-          extension = fileName.substring(fileName.indexOf("."),fileName.length());
           System.out.println("Give file path");
           filePath = br.readLine();
-          System.out.println("Give payload");
-          maxPayload = Integer.parseInt(br.readLine());
+          System.out.print("Give payload (Give value between 1-65500): ");
+          //To try mpike edw se periptwsh pou o xrhsths dwsei opoiadhpote allh timh ektos apo int
+          try {
+              maxPayload = Integer.parseInt(br.readLine());
+          }catch(NumberFormatException ex){
+              System.out.println("Wrong value! We support only integers");
+              input = true;
+          }
+
+          while(maxPayload<1 || maxPayload>65500 || input == true){
+              System.out.println("Wrong value for payload please give again!");
+              System.out.println("Give payload (Give value between 1-65500): ");
+              try {
+                  maxPayload = Integer.parseInt(br.readLine());
+                  input = false;
+              }catch(NumberFormatException ex){
+                  System.out.println("Wrong value! We support only integers");
+              }
+          }
+
           address = InetAddress.getByName(serverIP);
 
-      }else{
+      }else if(args.length == 5){
           serverIP = args[0];
           portNumber = Integer.parseInt(args[1]);
           fileName = args[2];
           extension = fileName.substring(fileName.indexOf("."),fileName.length());
           filePath = args[3];
-          maxPayload = Integer.parseInt(args[4]);
+          try {
+              maxPayload = Integer.parseInt(args[4]);
+          }catch(NumberFormatException ex){
+              System.out.println("Wrong value! We support only integers");
+              System.out.println("Give arguments again!");
+              System.out.println("Exiting...");
+              exit(0);
+          }
+          if(maxPayload>65500 || maxPayload<1){
+              System.out.println("Payload out of bounds!");
+              System.out.println("Give arguments again!");
+              System.out.println("Exiting...");
+              exit(0);
+          }
           //get a Datagram socket
           address = InetAddress.getByName(serverIP);
           System.out.println(fileName + "   " + filePath);
 
+      }else{
+          System.out.println("Wrong number of arguments!");
       }
 
-      ClientHandler clientHandler = new ClientHandler(address,portNumber,filePath,fileName,extension,maxPayload);
+      //Kanoume concat to file name kai to file path tou arxeioy poy peirame!
+      finalPath = filePath.concat("\\").concat(fileName);
+      //Vlepoume to extension tou file pou dwsame ws input wste to file pou 8a exoume san output na exei to idio extension
+      extension = fileName.substring(fileName.indexOf("."),fileName.length());
+
+      ClientHandler clientHandler = new ClientHandler(address,portNumber,finalPath,fileName,extension,maxPayload);
       clientHandler.start();
   }
 }
